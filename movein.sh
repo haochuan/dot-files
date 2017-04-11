406c9ba363f05d278f92658e664c244dfe97d289
Haochuan Liu <haochuan.liu@gmail.com>
2017-04-11 14:55:24 -0700

    add .gitignore_global

--- movein.sh
+++ movein.sh
@@ -5,3 +5,9 @@ echo "Staring moving dot-files from local to remote"
 echo "Moving .vimrc..."
 cp ~/.vimrc ./
 echo "Done"
+
+
+echo "Staring moving dot-files from local to remote"
+echo "Moving .gitignore_global..."
+cp ~/.gitignore_global ./
+echo "Done"
--- moveout.sh
+++ moveout.sh
@@ -4,3 +4,9 @@ echo "Staring moving dot-files from remote to your local..."
 echo "Moving .vimrc..."
 cp ./.vimrc ~/.
 echo "Done"
+
+
+echo "Staring moving dot-files from remote to your local..."
+echo "Moving .gitignore_global"
+cp ./.gitignore_global ~/.
+echo "Done"
