4ce9c8ed5ec49f50c4b42f0f910d93952981d3e6
haochuan <haochuan.liu@gmail.com>
2022-05-30 23:11:10 -0700

    remove nvim in repo

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@ REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BACKUP_DIR=~/.dotfiles_backup/$(date +%Y-%m-%d_%T)
 
 
-for file in $(find $REPO_DIR -maxdepth 1 -iname '.*' -not -path '*/.git')
+for file in $(find $REPO_DIR -maxdepth 1 -iname '.*' -not -path '*/.git' -not -path '*/nvim')
 do
   filename=$(basename $file)
   mkdir -p $BACKUP_DIR
@@ -17,9 +17,12 @@ do
 done
 
 # Copy custom nvchad config
-# if [ -d $REPO_DIR/nvim/ ]; then
-#   git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1; nvim
-# fi
+if ! [ -d $REPO_DIR/nvim/ ]; then
+  rm -rf ~/.cache/nvim
+  rm -rf ~/.local/share/nvim
+  rm -rf ~/.config/nvim
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1; nvim
+fi
 
 
 if ! [ -d $REPO_DIR/nvim/lua/custom/ ]; then
